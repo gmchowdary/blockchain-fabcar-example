1223172593ec3f03c5e0a28d70eae306d40a8f60
gmchowdary <geeth.chowdary.m@gmail.com>
2018-06-11 11:53:57 +0530

    initial commit

---
+++ api/controllers/student/create.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'Create',
+
+
+  description: 'Create student.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    return exits.success();
+
+  }
+
+
+};
